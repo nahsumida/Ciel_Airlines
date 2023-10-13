0dffececd9e9413e963213edefd97dd44dfba7d2
Natália Naomi Sumida <75488386+nahsumida@users.noreply.github.com>
2023-10-13 17:35:25 -0300

    insert and delete aeroporto

--- Ciel_Airlines_API/src/routers/aeroporto.ts
+++ Ciel_Airlines_API/src/routers/aeroporto.ts
@@ -39,4 +39,109 @@ aeroportoRouter.get("/listarAeroporto", async(req:any, res:any)=>{
         res.send(cr);  
     }
 });
+
+
+aeroportoRouter.delete("/excluirAeroporto", async(req:any, res:any)=>{
+
+    const idAeroporto = req.body.idAeroporto as number;
+   
+    let cr: CustomResponse = {
+      status: "ERROR",
+      message: "",
+      payload: undefined,
+    };
   
+    try{
+      const connection = await oracledb.getConnection({
+        user : process.env.ORACLE_DB_USER, 
+        password : process.env.ORACLE_DB_SECRET,
+        connectString : process.env.ORACLE_DB_CONN_STR
+      });
+  
+      let resDelete = await connection.execute(`DELETE AEROPORTO WHERE IDAEROPORTO = :1`, [idAeroporto]);
+      
+      await connection.commit();
+  
+      await connection.close();
+      
+      const rowsDeleted = resDelete.rowsAffected
+      if(rowsDeleted !== undefined &&  rowsDeleted === 1) {
+        cr.status = "SUCCESS"; 
+        cr.message = "Dado excluído.";
+      }else{
+        cr.message = "Dado não excluído. Verifique se o id informado está correto.";
+      }
+  
+    }catch(e){
+      if(e instanceof Error){
+        cr.message = e.message;
+        console.log(e.message);
+      }else{
+        cr.message = "Erro ao conectar ao oracle. Sem detalhes";
+      }
+    } finally {
+      res.send(cr);  
+    }
+  });
+  
+  aeroportoRouter.post("/inserirAeroporto", async(req:any, res:any)=>{
+    
+    const idCidade = req.body.idCidade as number;
+    const nomeAeroporto = req.body.nomeAeroporto as string;
+   
+    // correção: verificar se tudo chegou para prosseguir com o cadastro.
+    // verificar se chegaram os parametros
+    // VALIDAR se estão bons (de acordo com os critérios - exemplo: 
+    // não pode qtdeAssentos ser número e ao mesmo tempo o valor ser -5)
+  
+    // definindo um objeto de resposta.
+    let cr: CustomResponse = {
+      status: "ERROR",
+      message: "",
+      payload: undefined,
+    };
+   
+    let connection; 
+  
+    try{
+      connection = await oracledb.getConnection({
+        user : process.env.ORACLE_DB_USER, 
+        password : process.env.ORACLE_DB_SECRET,
+        connectString : process.env.ORACLE_DB_CONN_STR
+      });
+  
+      const cmdInsertAeroporto = `INSERT INTO AEROPORTO 
+      (IDAEROPORTO IDCIDADE, NOMEAEROPORTO)
+      VALUES (ID_AEROPORTO_SEQ.NEXTVAL, :1, :2)`
+  
+      const dados = [idCidade, nomeAeroporto]
+  
+      let resInsert = await connection.execute(cmdInsertAeroporto, dados);
+    
+      await connection.commit();
+  
+      const rowsInserted = resInsert.rowsAffected
+      console.log(rowsInserted)
+      if(rowsInserted !== undefined &&  rowsInserted === 1) {
+        cr.status = "SUCCESS"; 
+        cr.message = "Dado inserido.";
+      }
+  
+    }catch(e){
+      if(e instanceof Error){
+        cr.message = e.message;
+        console.log(e.message);
+      }else{
+        cr.message = "Erro ao conectar ao oracle. Sem detalhes";
+      }
+    } finally {
+      if (connection) {
+        try {
+          await connection.close({drop: true});
+        } catch (err) {
+          console.error(err);
+        }
+      }
+      res.send(cr);  
+    }
+});
