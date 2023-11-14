73e7d27fdc39d9e2f003e1d7d4eded43a0b63b1a
jessicakushidaa <jessicakushida2@gmail.com>
2023-11-13 21:41:23 -0300

    ,

--- Ciel_Airlines_BD/voo.sql
+++ Ciel_Airlines_BD/voo.sql
@@ -1,15 +1,22 @@
 
 -------------- VOO
-CREATE TABLE VOO (
-ID_VOO INTEGER NOT NULL PRIMARY KEY,
-DATA DATE NOT NULL ,
-TRECHO INTEGER NOT NULL,
-HORA_PARTIDA VARCHAR(5) NOT NULL,
-HORA_CHEGADA VARCHAR(5) NOT NULL,
-PRECO NUMBER(10) NOT NULL,
-AERONAVE VARCHAR(20) NOT NULL,
-FOREIGN KEY (TRECHO) REFERENCES TRECHO(ID_TRECHO),
-FOREIGN KEY (AERONAVE) REFERENCES AERONAVE(NUM_IDENTIFICACAO)
-);
+create table VOO
+(
+    ID_VOO       NUMBER       not null
+        primary key,
+    DATA         DATE         not null,
+    TRECHO       NUMBER       not null
+        constraint FK_TRECHO
+            references TRECHO
+                on delete cascade,
+    HORA_PARTIDA VARCHAR2(5)  not null,
+    HORA_CHEGADA VARCHAR2(5)  not null,
+    PRECO        NUMBER(10)   not null,
+    AERONAVE     VARCHAR2(20) not null
+        constraint FK_AERONAVE
+            references AERONAVE
+                on delete cascade
+)
+/
 
 CREATE SEQUENCE ID_VOO_SEQ START WITH 001 INCREMENT BY 1 MAXVALUE 9999 NOCYCLE NOCACHE;
