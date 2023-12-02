059f0be81f759ba9f962c7f89388c88d32681817
emillyfb21 <129964796+emillyfb21@users.noreply.github.com>
2023-12-02 15:22:41 -0300

    Update delete.js

--- Ciel_Airlines_API/dist/adapter/oraclebd/delete.js
+++ Ciel_Airlines_API/dist/adapter/oraclebd/delete.js
@@ -1,4 +1,4 @@
-"use strict"; // Ativa o modo estrito no JavaScript, tornando o código mais seguro e evitando certos tipos de erros.
+"use strict"; 
 
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
@@ -9,7 +9,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-// As linhas acima definem um utilitário chamado __awaiter para simplificar o uso de Promises com a sintaxe async/await.
 
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
@@ -18,28 +17,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.executeDeleteByID = void 0;
 const oracledb_1 = __importDefault(require("oracledb"));
 const config_1 = require("./config");
-//deleta todos os dados de um id especifico
 const executeDeleteByID = (table, id) => __awaiter(void 0, void 0, void 0, function* () {
     let resp = { result: undefined, err: null };
-// Semelhante ao __awaiter, isso define um utilitário para importar módulos que podem ter diferentes formatos de módulo.
 
 Object.defineProperty(exports, "__esModule", { value: true });
-// Esta linha é gerada automaticamente pelo TypeScript para indicar que o arquivo é um módulo TypeScript.
-
 exports.executeDeleteByID = void 0;
-// Essa linha exporta uma função chamada executeDeleteByID. Essa função é definida mais adiante no código.
 
 const oracledb_1 = __importDefault(require("oracledb"));
-// Importa o pacote 'oracledb' que fornece acesso ao banco de dados Oracle.
 
 const config_1 = require("./config");
-// Importa um módulo chamado 'config' de um arquivo local chamado 'config.js'.
-
 const executeDeleteByID = (table, id) => __awaiter(void 0, void 0, void 0, function* () {
-    // Define uma função assíncrona chamada executeDeleteByID com parâmetros table e id.
 
     let resp = { result: undefined, err: null };
-    // Declara um objeto chamado `resp` para armazenar o resultado da operação de exclusão.
 
     let connection;
     console.log("cheguei aqui");
@@ -51,23 +40,15 @@ const executeDeleteByID = (table, id) => __awaiter(void 0, void 0, void 0, funct
         yield connection.commit();
         yield connection.close();
         const rowsDeleted = resDelete.rowsAffected;
-        // Estabelece uma conexão com o banco de dados Oracle usando as informações de conexão definidas em 'config_1.oraConnAttribs'.
 
         let deleteString = `DELETE ` + table + ` WHERE ID_` + table + ` = ` + id;
-        // Cria uma string SQL para a operação de exclusão com base no nome da tabela e no ID fornecido.
 
         console.log(deleteString);
         let resDelete = yield connection.execute(deleteString);
-        // Executa a operação de exclusão no banco de dados.
-
         yield connection.commit();
-        // Confirma as alterações feitas na transação.
-
         yield connection.close();
-        // Fecha a conexão com o banco de dados.
-
         const rowsDeleted = resDelete.rowsAffected;
-        // Obtém o número de registros afetados pela operação de exclusão.
+   
 
         if (rowsDeleted !== undefined && rowsDeleted === 1) {
             resp.result = rowsDeleted;
@@ -85,7 +66,5 @@ const executeDeleteByID = (table, id) => __awaiter(void 0, void 0, void 0, funct
         return resp;
     }
 });
-// Exporta a função executeDeleteByID que deleta registros com base em uma tabela e ID específicos.
 
 exports.executeDeleteByID = executeDeleteByID;
-// Exporta a função para ser utilizada em outros módulos ou scripts.
